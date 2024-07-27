16e540520f82412a8fefb79e8bbeb606718351d7
mohammadsaeediaf <alisaadati18000@gmail.com>
2024-07-27 19:42:26 +0330

    Check user selection and show the answer

--- src/components/options.js
+++ src/components/options.js
@@ -1,9 +1,23 @@
-function option({ question }) {
-  console.log(question);
+function option({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+  console.log(hasAnswered);
+
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} 
+          ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          disabled={hasAnswered}
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
